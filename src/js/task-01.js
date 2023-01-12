2a97bfa701d8d03e98dd7d8a68ce368d417ea654
e25viktor05 <107816900+e25viktor05@users.noreply.github.com>
2023-01-12 19:46:40 +0200

    add

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,14 @@
-const findCategory = document.querySelectorAll('.item');                                    console.log('Number of categories:',findCategory.length);
-const newArray = [...findCategory].map(element => Category:${element.children[0].textContent}Elements: ${element.children[1].children.length}).join('');  
-console.log(newArray);
-
+// const findCategory = document.querySelectorAll('.item');                                    console.log('Number of categories:',findCategory.length);
+// const newArray = [...findCategory].map(element => Category:${element.children[0].textContent}Elements: ${element.children[1].children.length}).join('');  
+// console.log(newArray);
+const findCategory = document.querySelectorAll('.item');                                    
+ console.log('Number of categories:',findCategory.length);
+const items = [...findCategory];
+items.forEach((element) =>{
+    console.log("Category:", element.firstElementChild.textContent);
+    console.log("Elements:", element.lastElementChild.childElementCount);
+    
+});
 
 // const listEl = document.querySelector("#categories");
 // console.log("Number of categories:", listEl.childElementCount);
